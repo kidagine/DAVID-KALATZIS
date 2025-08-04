073931a5c40cacc97aca67e976900e6e5e8de0e0
David Kalatzis <david.kalatzis@lego.com>
2025-08-04 14:22:45 +0200

    add misans font

--- david-kalatzis/src/App.jsx
+++ david-kalatzis/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
+import "./fonts.css";
 
 function App() {
   return (
     <>
+      <div className="component-container">
+        <div>hi</div>
+      </div>
       <div className="component-container">
         <Footer />
       </div>
--- david-kalatzis/src/components/Footer/Footer.jsx
+++ david-kalatzis/src/components/Footer/Footer.jsx
@@ -3,10 +3,15 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <div className="component-container">
-        <div className="footer-text">©2025 David Kalatzis.</div>
+      <div className="footer-inner-container">
         <div className="footer-text">
-          Connection for Business:davidkalatzis@gmail.com
+          ©2025 David Kalatzis. All Rights Reserved
+        </div>
+        <div className="footer-text">
+          Connection for Business:{" "}
+          <a className="footer-link" href="mailto:davidkalatzis@gmail.com">
+            davidkalatzis@gmail.com
+          </a>
         </div>
       </div>
     </div>
