b8e90e5513ea0b9e8b98142ce4815fa23dd39e59
David Kalatzis <david.kalatzis@lego.com>
2025-08-04 14:53:58 +0200

    update

--- david-kalatzis/src/components/TopBar/TopBar.jsx
+++ david-kalatzis/src/components/TopBar/TopBar.jsx
@@ -6,8 +6,8 @@ function TopBar() {
       <div className="top-bar-inner-container">
         <div className="top-bar-icon">icon</div>
         <div className="top-bar-buttons-container">
-          <button>Contact</button>
-          <button>CV</button>
+          <button className="top-bar-button">Contact</button>
+          <button className="top-bar-button">CV</button>
         </div>
       </div>
     </div>
