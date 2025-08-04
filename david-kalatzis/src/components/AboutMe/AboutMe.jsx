8fb5fa185b7a038bc6b76e535cf807fa82b46d85
David Kalatzis <david.kalatzis@lego.com>
2025-08-04 19:52:21 +0200

    up

--- david-kalatzis/src/components/AboutMe/AboutMe.jsx
+++ david-kalatzis/src/components/AboutMe/AboutMe.jsx
@@ -3,7 +3,15 @@ import "./AboutMe.css";
 function AboutMe() {
   return (
     <div className="about-me">
-      <div>hi names david</div>
+      <div className="about-me-picture">picture</div>
+      <div className="about-me-info-container">
+        <div className="about-me-name">David Kalatzis</div>
+        <div className="about-me-description">hi names david</div>
+        <div className="about-me-icons-containers">
+          <div>github</div>
+          <div>linkedin</div>
+        </div>
+      </div>
     </div>
   );
 }
