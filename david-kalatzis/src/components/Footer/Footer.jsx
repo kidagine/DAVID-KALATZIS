3c647440b91d23bb4bb9c05c9fc2c0ec511c8ef1
David Kalatzis <david.kalatzis@lego.com>
2025-08-04 13:56:47 +0200

    Update footer

--- david-kalatzis/src/components/Footer/Footer.jsx
+++ david-kalatzis/src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">background</div>;
+  return (
+    <div className="footer">
+      <div className="component-container">
+        <div className="footer-text">©2025 David Kalatzis.</div>
+        <div className="footer-text">
+          Connection for Business:davidkalatzis@gmail.com
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
