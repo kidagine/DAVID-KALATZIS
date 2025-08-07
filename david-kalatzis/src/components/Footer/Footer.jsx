6ee194da6cd67882722d88e09af55c87ac16ef1b
David Kalatzis <david.kalatzis@lego.com>
2025-08-07 20:44:25 +0200

    up

--- david-kalatzis/src/components/Footer/Footer.jsx
+++ david-kalatzis/src/components/Footer/Footer.jsx
@@ -1,17 +1,31 @@
 import "./Footer.css";
 
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="footer">
-      <div className="footer-inner-container">
-        <div className="footer-text">
-          ©2025 David Kalatzis. All Rights Reserved
+      <div className="footer-container">
+        <div className="footer-info-container">
+          <div className="footer-text">
+            ©2025 David Kalatzis. All Rights Reserved
+          </div>
+          <div className="footer-text">
+            Connection for Business:{" "}
+            <a className="footer-link" href="mailto:davidkalatzis@gmail.com">
+              davidkalatzis@gmail.com
+            </a>
+          </div>
         </div>
-        <div className="footer-text">
-          Connection for Business:{" "}
-          <a className="footer-link" href="mailto:davidkalatzis@gmail.com">
-            davidkalatzis@gmail.com
-          </a>
+        <div className="footer-up-nav-container">
+          <button className="footer-up-nav" onClick={scrollToTop}>
+            Up
+          </button>
         </div>
       </div>
     </div>
