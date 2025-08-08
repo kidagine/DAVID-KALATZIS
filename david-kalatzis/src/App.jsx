b01b478c38c5053d5030ff2bcf1eb337302f110c
David Kalatzis <david.kalatzis@lego.com>
2025-08-08 10:57:29 +0200

    up

--- david-kalatzis/src/App.jsx
+++ david-kalatzis/src/App.jsx
@@ -11,6 +11,17 @@ function App() {
   const aboutRef = useRef(null);
   const observerRef = useRef(null);
 
+  const images = [
+    "https://i.pinimg.com/736x/87/60/46/876046238a2fadd46b9556d90bc3e07a.jpg",
+    "https://i.pinimg.com/736x/a7/28/be/a728be564049f0cc4425619858fa83f4.jpg",
+    "https://i.pinimg.com/736x/0f/0b/51/0f0b514d4e3471edc35ee39669bd5001.jpg",
+    "https://i.pinimg.com/736x/6f/32/17/6f3217d9e1c4cfc489ae389f513fdebf.jpg",
+    "https://i.pinimg.com/1200x/5b/7e/69/5b7e692586039c07614813d7bdf3b6dd.jpg",
+    "https://i.pinimg.com/1200x/10/0b/1c/100b1c8123c707c9953d5a8211de4bea.jpg",
+    "https://i.pinimg.com/1200x/15/b7/9e/15b79ed860dfc76c61496383c08d60cf.jpg",
+    "https://i.pinimg.com/1200x/e0/9f/e8/e09fe8a8d4f25e12083117f8558df9f4.jpg",
+  ];
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -38,7 +49,7 @@ function App() {
         <TopBar isSticky={isSticky} />
       </div>
       <div className="component-container">
-        <Content />
+        <Content images={images} />
       </div>
       <Footer />
     </div>
