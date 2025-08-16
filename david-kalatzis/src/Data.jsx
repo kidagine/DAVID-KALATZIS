c3e1a9ffb3af5cfd606eb0835b89bb43d927bf99
David Kalatzis <david.kalatzis@lego.com>
2025-08-16 10:22:44 +0200

    up

--- david-kalatzis/src/Data.jsx
+++ david-kalatzis/src/Data.jsx
@@ -2,58 +2,65 @@ export const personalData = [
   {
     image:
       "https://i.pinimg.com/1200x/3e/ff/31/3eff31835905d618fa69adef49923bdf.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Custom netcode fighting game",
+    date: "Unity | C#",
+    company: "Darklings",
   },
   {
     image:
       "https://i.pinimg.com/736x/20/52/1d/20521ded456bdd1d2b15ca94637a50d9.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "FPS IK controller",
+    date: "Unity | C#",
+    company: "Full-body awareness",
   },
   {
     image:
       "https://i.pinimg.com/1200x/8d/30/01/8d3001a348320c4a64a538c7d1640d21.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Game-Jam in 2 days",
+    date: "Unity | C#",
+    company: "Blite",
   },
   {
     image:
       "https://i.pinimg.com/736x/30/e3/37/30e3379a1e9f5745d01cd91ff9a968b2.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Small game prototype",
+    date: "Unity | C#",
+    company: "Specter",
   },
   {
     image:
       "https://i.pinimg.com/736x/3f/18/1f/3f181fb1d1e9b5f11a74b9492cbd23c8.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "In development indie",
+    date: "Unity | C#",
+    company: "Project Hero",
   },
   {
     image:
       "https://i.pinimg.com/736x/30/22/66/302266806087fa7320f942c66004f2f9.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Boilerplate for Unity projects",
+    date: "Unity | C#",
+    company: "Chronos Boilerplate",
   },
   {
     image:
       "https://i.pinimg.com/736x/00/f3/af/00f3af2a44bcef1f1b9dffc8aea1439a.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Tiny projects with pure C++",
+    date: "C++ | SDL | OGL",
+    company: "Open GL Projects",
   },
   {
     image:
       "https://i.pinimg.com/736x/ab/dc/93/abdc93b33b06bcc57f2d9cb8781e8221.jpg",
-    title: "",
-    date: "",
-    company: "",
+    title: "Winner project in Uni",
+    date: "Unity | C#",
+    company: "Coma",
+  },
+  {
+    image:
+      "https://i.pinimg.com/736x/ab/dc/93/abdc93b33b06bcc57f2d9cb8781e8221.jpg",
+    title: "Replicas of existing games",
+    date: "Unity | C#",
+    company: "Small Replicas",
   },
 ];
 
