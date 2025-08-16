ead4cd9549fa4ca59e840e873bcc2c59b5bf53bb
David Kalatzis <david.kalatzis@lego.com>
2025-08-16 09:30:21 +0200

    up

--- david-kalatzis/src/components/TopBar/TopBar.jsx
+++ david-kalatzis/src/components/TopBar/TopBar.jsx
@@ -17,65 +17,73 @@ function TopBar({ isSticky, selectedValue, setSelectedValue }) {
         <div
           className={`top-bar-inner-container ${isSticky ? "is-sticky" : ""}`}
         >
-          <div className="top-bar-home-container">
-            <HouseIcon className="top-bar-home-icon" />
-          </div>
-          <div className="top-bar-selector-group">
-            <div className="top-bar-selector-wrapper">
-              <div
-                className="top-bar-selector-indicator"
-                style={{ transform: `translateX(${selectedValue * 100}%)` }}
-              />
-              <button
-                onClick={() => {
-                  scrollToTop();
-                  setSelectedValue(0);
-                }}
-                className={`top-bar-selector-button ${
-                  selectedValue === 0 ? "m--active" : ""
-                }`}
-              >
-                Professional
-              </button>
-              <button
-                onClick={() => {
-                  scrollToTop();
-                  setSelectedValue(1);
-                }}
-                className={`top-bar-selector-button ${
-                  selectedValue === 1 ? "m--active" : ""
-                }`}
-              >
-                Personal
-              </button>
-              <button
-                onClick={() => {
-                  scrollToTop();
-                  setSelectedValue(2);
-                }}
-                className={`top-bar-selector-button ${
-                  selectedValue === 2 ? "m--active" : ""
-                }`}
-              >
-                Me
-              </button>
+          <div className="top-bar-item">
+            <div className="top-bar-home-container">
+              <HouseIcon className="top-bar-home-icon" />
             </div>
           </div>
-          <div className="top-bar-buttons-container">
-            <div className={"top-bar-button-shadow"}>
-              <a
-                href="mailto:davidkalatzis@gmail.com"
-                className="top-bar-button top-bar-button-left"
-              >
-                Email Me!
-                <EmailIcon className="top-bar-button-icon" />
-              </a>
+
+          <div className="top-bar-item">
+            <div className="top-bar-selector-group">
+              <div className="top-bar-selector-wrapper">
+                <div
+                  className="top-bar-selector-indicator"
+                  style={{ transform: `translateX(${selectedValue * 100}%)` }}
+                />
+                <button
+                  onClick={() => {
+                    scrollToTop();
+                    setSelectedValue(0);
+                  }}
+                  className={`top-bar-selector-button ${
+                    selectedValue === 0 ? "m--active" : ""
+                  }`}
+                >
+                  Professional
+                </button>
+                <button
+                  onClick={() => {
+                    scrollToTop();
+                    setSelectedValue(1);
+                  }}
+                  className={`top-bar-selector-button ${
+                    selectedValue === 1 ? "m--active" : ""
+                  }`}
+                >
+                  Personal
+                </button>
+                <button
+                  onClick={() => {
+                    scrollToTop();
+                    setSelectedValue(2);
+                  }}
+                  className={`top-bar-selector-button ${
+                    selectedValue === 2 ? "m--active" : ""
+                  }`}
+                >
+                  Me
+                </button>
+              </div>
             </div>
-            <div className={"top-bar-button-shadow"}>
-              <a className="top-bar-button top-bar-button-right">
-                CV
-                <ArrowUpRightIcon className="top-bar-button-icon" />
-              </a>
+          </div>
+
+          <div className="top-bar-item">
+            <div className="top-bar-buttons-container">
+              <div className={"top-bar-button-shadow"}>
+                <a
+                  href="mailto:davidkalatzis@gmail.com"
+                  className="top-bar-button top-bar-button-left"
+                >
+                  Email Me!
+                  <EmailIcon className="top-bar-button-icon" />
+                </a>
+              </div>
+              <div className={"top-bar-button-shadow"}>
+                <a className="top-bar-button top-bar-button-right">
+                  CV
+                  <ArrowUpRightIcon className="top-bar-button-icon" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
