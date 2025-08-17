46cd3067f1cce5ce541f8a96cee6d639cc6f2a29
David Kalatzis <david.kalatzis@lego.com>
2025-08-17 22:01:52 +0200

    up

--- david-kalatzis/src/components/About/About.jsx
+++ david-kalatzis/src/components/About/About.jsx
@@ -1,7 +1,18 @@
 import "./About.css";
 
 function About() {
-  return <div className="content">about</div>;
+  return (
+    <div className="about-me">
+      <div className="about-me-section">
+        <div className="about-me-title">Where I'm Based</div>
+        <div className="about-me-description">
+          I’m currently based in Denmark, working at the LEGO Campus in Billund.
+          In my previous life, I’ve moved around everywhere including cities
+          like Seoul, Singapore, Vancouver, Toronto and DC.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
