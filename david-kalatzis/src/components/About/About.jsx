fe4bb2abc4da2cec83c7a62227e9c738eb1d3a39
David Kalatzis <david.kalatzis@lego.com>
2025-08-19 22:14:50 +0200

    up

--- david-kalatzis/src/components/About/About.jsx
+++ david-kalatzis/src/components/About/About.jsx
@@ -1,5 +1,8 @@
 import "./About.css";
 import BasedIn from "../../assets/svgs/BasedIn.svg?react";
+import Education from "../../assets/svgs/Education.svg?react";
+import Work from "../../assets/svgs/Work.svg?react";
+import Tools from "../../assets/svgs/Tools.svg?react";
 import { aboutData } from "../../data/AboutData";
 
 function About() {
@@ -9,14 +12,14 @@ function About() {
         <div className="about-me-top">
           <div className="about-me-based-in about-me-part ">
             <div className="about-me-title-group">
-              {/* <BasedIn className="about-me-icon" /> */}
+              <BasedIn className="about-me-icon" />
               <div className="about-me-title">{aboutData.location}</div>
             </div>
             <div className="about-me-description">{aboutData.locationText}</div>
           </div>
           <div className="about-me-education about-me-part ">
             <div className="about-me-title-group">
-              {/* <BasedIn className="about-me-icon" /> */}
+              <Education className="about-me-icon" />
               <div className="about-me-title">{aboutData.education}</div>
             </div>
             <div className="about-me-description">
@@ -28,13 +31,20 @@ function About() {
         <div className="about-me-bottom">
           <div className="about-me-work about-me-part ">
             <div className="about-me-title-group">
-              {/* <BasedIn className="about-me-icon" /> */}
+              <Work className="about-me-icon" />
               <div className="about-me-title">{aboutData.work}</div>
             </div>
             <div className="about-me-list">
               {aboutData.workItems.map((item, index) => (
                 <div className="about-me-list-item" key={index}>
-                  <div className="about-me-description">{item.title}</div>
+                  <div className="about-me-list-description">
+                    <div className="about-me-description">
+                      {item.title} |{" "}
+                      <a className="about-me-description" href={item.url}>
+                        {item.company}
+                      </a>
+                    </div>
+                  </div>
                   <div className="about-me-description">{item.time}</div>
                 </div>
               ))}
@@ -42,7 +52,7 @@ function About() {
           </div>
           <div className="about-me-tools about-me-part ">
             <div className="about-me-title-group">
-              {/* <BasedIn className="about-me-icon" /> */}
+              <Tools className="about-me-icon" />
               <div className="about-me-title">{aboutData.tools}</div>
             </div>
             <div className="about-me-list">
