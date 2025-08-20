d7b40289bc465cb481a66dc3a2e2fafa52a467be
David Kalatzis <david.kalatzis@lego.com>
2025-08-20 14:03:38 +0200

    uup

--- david-kalatzis/src/components/ContentDetails/ContentDetails.jsx
+++ david-kalatzis/src/components/ContentDetails/ContentDetails.jsx
@@ -3,14 +3,21 @@ import "./ContentDetails.css";
 function ContentDetails({ data }) {
   return (
     <div className="content-details">
-      <h2>{data.title}</h2>
-      <p>
-        <strong>Company:</strong> {data.company}
-      </p>
-      <p>
-        <strong>Date:</strong> {data.date}
-      </p>
-      <img src={data.image} alt={data.title} className="details-image" />
+      <div className="content-details-main-section">
+        <div className="content-details-main-info">
+          <div className="content-details-main-company">{data.company}</div>
+          <div className="content-details-main-title">{data.title}</div>
+        </div>
+        <div className="content-details-main-date">{data.date}</div>
+      </div>
+      <div className="content-details-section">
+        <div className="content-details-title">{"salkdjsalkdjsalkd"}</div>
+        <div className="content-details-description">
+          {"sdhasjldhasjdaskdha"}
+        </div>
+      </div>
+      <div className="content-details-section">s</div>
+      <div className="content-details-section">s</div>
     </div>
   );
 }
