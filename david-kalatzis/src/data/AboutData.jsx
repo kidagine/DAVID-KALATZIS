959b6b95eceb4b65c9a066843798624f094d7593
David Kalatzis <david.kalatzis@lego.com>
2025-08-20 16:33:34 +0200

    up

--- david-kalatzis/src/data/AboutData.jsx
+++ david-kalatzis/src/data/AboutData.jsx
@@ -4,45 +4,6 @@ export const aboutData = {
   education: "Education:",
   educationText: "Erhvervsakademi SydVest Software Development",
   work: "Work:",
-  workItems: [
-    {
-      title: "Frontend Developer",
-      company: "LEGO",
-      time: "2025 - Present",
-      url: "https://education.lego.com/en-us/",
-    },
-    {
-      title: "Lead Game Programmer",
-      company: "Inner Engine",
-      time: "2023 - 2025",
-      url: "https://store.steampowered.com/app/2629170/FreeJack_Online/",
-    },
-    {
-      title: "Lead Engineer",
-      company: "Ampersand Associates",
-      time: "2022 - 2023",
-      url: "https://amper.dk/",
-    },
-    { title: "Game Programmer", company: "Undisclosed", time: "2022 - 2023" },
-    {
-      title: "Fullstack Developer Intern",
-      company: "Grumsen Development",
-      time: "2021",
-      url: "https://grumsendev.dk/",
-    },
-    {
-      title: "Game Engineer Contractor",
-      company: "Oneiros",
-      time: "2020",
-      url: "https://oneirosvr.com/",
-    },
-    {
-      title: "Game Engineer Intern",
-      company: "Amunet Studio",
-      time: "2020",
-      url: "https://www.amunetstudio.com/",
-    },
-  ],
   tools: "Tools:",
   toolsItems: ["Unity", "Unreal", "Godot", "React", "C#", "C++", "JS / TS"],
 };
