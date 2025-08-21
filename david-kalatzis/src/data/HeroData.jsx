ffe34d575ce228f3c0dd2fe31ecbd3d1d53b8604
David Kalatzis <david.kalatzis@lego.com>
2025-08-21 08:56:14 +0200

    up

--- david-kalatzis/src/data/HeroData.jsx
+++ david-kalatzis/src/data/HeroData.jsx
@@ -2,5 +2,5 @@ export const heroData = {
   name: "David Kalatzis",
   github: "https://github.com/kidagine",
   linkedin: "https://www.linkedin.com/in/davidkalatzis/",
-  description: `I'm a developer with a degree and a wealth of work experience. Focused on bringing joyful moments through digitally interactive means, with a keen eye for a quality experience to all users.\n Located in Denmark working at LEGO.`,
+  description: `I'm a developer with a degree and a wealth of work experience. Focused in bringing joyful moments through digitally interactive means, with a keen eye for a quality experience to all users.\n Located in Denmark working at LEGO.`,
 };
