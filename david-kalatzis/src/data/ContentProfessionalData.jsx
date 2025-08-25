780498a1ad22621f1c72d4b509cf2035d9bbaa57
David Kalatzis <david.kalatzis@lego.com>
2025-08-25 18:32:51 +0200

    up

--- david-kalatzis/src/data/ContentProfessionalData.jsx
+++ david-kalatzis/src/data/ContentProfessionalData.jsx
@@ -4,6 +4,10 @@ import lego3 from "../assets/images/Lego3.jpeg";
 import lego4 from "../assets/images/Lego4.png";
 import freejack from "../assets/images/FreeJack.png";
 import aji from "../assets/images/AJI.png";
+import oneiros from "../assets/images/Oneiros.png";
+import ampersand from "../assets/images/Ampersand.png";
+import grumsen from "../assets/images/Grumsen.png";
+import amunet from "../assets/images/Amunet.png";
 
 export const professionalData = [
   {
@@ -66,14 +70,6 @@ export const professionalData = [
     company: "AJI Productions",
   },
   {
-    image:
-      "https://i.pinimg.com/736x/6f/32/17/6f3217d9e1c4cfc489ae389f513fdebf.jpg",
-    title: "Game Engineer Contractor",
-    company: "Oneiros",
-    date: "2020",
-    url: "https://oneirosvr.com/",
-  },
-  {
     image: freejack,
     title: "Lead Game Programmer",
     company: "Inner Engine",
@@ -81,24 +77,71 @@ export const professionalData = [
     url: "https://store.steampowered.com/app/2629170/FreeJack_Online/",
   },
   {
-    image:
-      "https://i.pinimg.com/1200x/5b/7e/69/5b7e692586039c07614813d7bdf3b6dd.jpg",
+    image: oneiros,
+    title: "Game Engineer Contractor",
+    company: "Oneiros",
+    date: "2020",
+    url: "https://oneirosvr.com/",
+    details: [
+      {
+        section: [
+          {
+            type: "_text",
+            title: "Contribution:",
+            body:
+              "Worked at the LEGO Education department of LEGO, heavily contributing to the website, by developing new pages and refining existing ones." +
+              " Collaborated with the rest of the dev team in the front and back end of things as well as designers to provide the playfulness and quality that LEGO is known for.",
+          },
+        ],
+      },
+      {
+        section: [
+          {
+            type: "_video",
+            url: "https://www.youtube.com/embed/5kc4tkuaiDI?enablejsapi=1",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    image: ampersand,
+    title: "Lead Engineer",
+    company: "Ampersand Associates",
+    date: "2022 - 2023",
+    url: "https://amper.dk/",
+    details: [
+      {
+        section: [
+          {
+            type: "_text",
+            title: "Contribution:",
+            body:
+              "Worked at the LEGO Education department of LEGO, heavily contributing to the website, by developing new pages and refining existing ones." +
+              " Collaborated with the rest of the dev team in the front and back end of things as well as designers to provide the playfulness and quality that LEGO is known for.",
+          },
+        ],
+      },
+      {
+        section: [
+          {
+            type: "_video",
+            url: "https://www.youtube.com/embed/5kc4tkuaiDI?enablejsapi=1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    image: grumsen,
     title: "Fullstack Developer Intern",
     company: "Grumsen Development",
     date: "2021",
     url: "https://grumsendev.dk/",
   },
   {
-    image:
-      "https://i.pinimg.com/736x/0f/0b/51/0f0b514d4e3471edc35ee39669bd5001.jpg",
-    title: "Lead Engineer",
-    company: "Ampersand Associates",
-    date: "2022 - 2023",
-    url: "https://amper.dk/",
-  },
-  {
-    image:
-      "https://i.pinimg.com/1200x/10/0b/1c/100b1c8123c707c9953d5a8211de4bea.jpg",
+    image: amunet,
     title: "Game Engineer Intern",
     company: "Amunet Studio",
     date: "2020",
